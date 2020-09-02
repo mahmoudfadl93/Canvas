8f9bc29451fa47aeff038e5e2fa9e7cd268feef4
mahmoud.fadl.FLAIRSTECH <mfadl4201@gmail.com>
2020-09-02 23:50:52 +0200

    transSkills files

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  //*************************
+  //                AOS
+  //************************
+  AOS.init({
+    offset: 100,
+    duration: 600,
+    easing: "ease-in-sine",
+    delay: 100,
+    /*disable: 'mobile'*/ // accepts following values: 'phone', 'tablet',
+  });
+  //*************************
+  //                AOS
+  //************************
+});
